183449320f56eabbf5afcea5895d9e8689804370
xNickolas <nickolsctt@gmail.com>
2020-04-14 14:27:58 -0300

    New search form

---
+++ src/app/search-weather/search-weather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-weather',
+  templateUrl: './search-weather.component.html',
+  styleUrls: ['./search-weather.component.css']
+})
+export class SearchWeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
